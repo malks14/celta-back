46c01caf31e01cc7d11b76a6e730a1316a5b453e
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-16 16:36:35 -0300

    fix: new route user

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const userController = require("../controllers/user");
-const { body } = require("express-validator/check");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
@@ -10,7 +10,7 @@ router.post(
   "/add-user",
   [
     body("name").isString().isLength({ min: 3 }).trim(),
-    body("lname").isString().isLength({ min: 3 }).trim(),
+    body("lName").isString().isLength({ min: 3 }),
     body("password").isString().isLength({ min: 6 }).trim(),
   ],
   userController.postAddUser
