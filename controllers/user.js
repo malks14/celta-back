3f89927b5c642afb4936580e18a031c3d221fd1a
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-16 16:33:29 -0300

    fix: new user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,19 +1,31 @@
 
-exports.postAddUser = (req, res, next) => {
+const bcrypt = require("bcryptjs");
+const User = require('../models/user')
+
+
+exports.postAddUser =  (req, res, next) => {
   const name = req.body.name;
-  const lname = req.body.lname;
+  const lName = req.body.lName;
   const password = req.body.password
-  req.user
-    .createUser({
-      name,
-      lname,
-      password,
-    })
-    .then(result => {
-      console.log(result);
-      console.log('Created user');
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  const region = req.body.region;
+  const country = req.body.country
+  if (name === "" || lName === "" || password === "" || region === "" || country === "") {
+    return res.status(400).json({ msg: "Debe llenar todos los campos" });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ msg: "La contraseña es muy corta. Debe ser minimo 6 caracteres" });
+  }
+  bcrypt.hash(password, 12)
+  .then((hashedPsw) => {
+    const user = new User(null, name, lName, hashedPsw, region, country)
+    return user.save()
+  })
+  .then(() => {
+    return res.status(200).json({ msg: "Se ha creado el usuario" });
+  }).catch(err => {
+    console.log(err);
+    return res.status(500).json({ msg: "No se puedo crear el usuario" });
+  })
+
 };
