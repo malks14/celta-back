8e1f298a1e1775ec4584d3183017e0da6cd3eb4a
malks14 <73920001+malks14@users.noreply.github.com>
2022-11-18 13:53:15 -0300

    new repo

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'celtadb',
-    password: 'slipknot1'
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD
 });
 
 module.exports = connection.promise()
